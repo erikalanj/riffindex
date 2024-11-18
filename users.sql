7e19e800e2abb419e4c688d0d4659c3b071df743
erikjensen <erikj@my.ccsu.edu>
2024-11-18 14:50:21 -0500

    blah

--- users.sql
+++ users.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('casual', 'fan', 'superfan', 'admin') NOT NULL,
+    role ENUM('member' 'admin') NOT NULL,
     phone VARCHAR(15)
 );
 
